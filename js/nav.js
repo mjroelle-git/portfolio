c5b32a692eb6c1374ef175a46894c2fa50480178
mjroelle <mjroelle@iu.edu>
2026-07-08 16:25:20 +0800

    Claude first pass

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,14 @@
 function openNav() {
-    document.querySelector('#mySidenav').style.width = "250px"; 
-    document.querySelector('.all-over-bkg').classList.add('is-visible');
-  }
-  
-  function closeNav() {
-    document.querySelector('#mySidenav').style.width = "0"; 
-    document.querySelector('.all-over-bkg').classList.remove('is-visible');
-  }
-  
-  document.querySelector('.openbtn').addEventListener('click', openNav);
-  document.querySelector('.closebtn').addEventListener('click', closeNav);
+  document.querySelector('#mySidenav').style.width = "250px";
+  document.querySelector('.all-over-bkg').classList.add('is-visible');
+}
+
+function closeNav() {
+  document.querySelector('#mySidenav').style.width = "0";
+  document.querySelector('.all-over-bkg').classList.remove('is-visible');
+}
+
+document.querySelector('.openbtn')?.addEventListener('click', openNav);
+document.querySelector('.closebtn')?.addEventListener('click', closeNav);
+// Clicking the dark overlay also closes the menu
+document.querySelector('.all-over-bkg')?.addEventListener('click', closeNav);
